15fb7ddbecc0ff660098eb07104537ff9d426e43
kazaky <kazak@microfocus.com>
2019-05-26 17:44:05 +0300

    fixed most issues

--- suffix_tree.go
+++ suffix_tree.go
@@ -74,18 +74,34 @@ func FindInsertionBranch(rootNode *SuffixNode, word []byte) (cursorNode *SuffixN
 	for index, char = range word {
 		isFound, cursorNode = queryChildren(cursorNode, char)
 		if isFound == false {
-			//We found the leaf node where the suffix should be inserted.
+			//We found the leaf Node where the suffix should be inserted.
 			break
 		}
 	}
 	return cursorNode, index, isFound
 }
 
-func (tree *SuffixTree) LookupWord(word []byte) (*SuffixNode, int, int, bool) {
-	return LookupWord(&tree.root, word)
+type Match struct {
+	Node       *SuffixNode
+	StartIndex int
+	EndIndex   int
+}
+
+func (tree *SuffixTree) LookupString(searchString []byte) (matches []Match, isFound bool) {
+	for _, word := range SegmentString(searchString) {
+		node, startIndex, endIndex, isFound := LookupWord(&tree.root, word)
+		if isFound {
+			matches = append(matches, Match{
+				node, startIndex, endIndex,
+			})
+		}
+	}
+	return matches, len(matches) > 0
+
 }
 
 func LookupWord(rootNode *SuffixNode, word []byte) (*SuffixNode, int, int, bool) {
+
 	for startIndex := 0; startIndex < len(word); startIndex++ {
 		cursorWord := word[startIndex:]
 		node, index, isFound := FindInsertionBranch(rootNode, cursorWord)
@@ -154,19 +170,48 @@ func NewSuffixTree() *SuffixTree {
 	}
 }
 
+func printChildren(t *SuffixNode, pre string) {
+	for _, cursorNode := range t.Children {
+		children := cursorNode.Children
+		if len(children) == 0 {
+			fmt.Println("╴", string(cursorNode.Value))
+			return
+		}
+		fmt.Println("┐", string(cursorNode.Value))
+		last := len(children) - 1
+		for _, ch := range children[:last] {
+			fmt.Print(pre, "├─")
+			printChildren(ch, pre+"│ ")
+		}
+		fmt.Print(pre, "└─")
+		printChildren(children[last], pre+"  ")
+	}
+}
+
+func vis(t *SuffixNode) {
+	if len(t.Children) == 0 {
+		fmt.Println("<empty tree>")
+		return
+	}
+	printChildren(t, "")
+}
+
 func main() {
 	tree := NewSuffixTree()
-	tree.InsertString([]byte("cgi"))
-	tree.InsertString([]byte("rtcgi"))
-	tree.InsertString([]byte("cgigi"))
+	tree.InsertString([]byte("boris tries to take over the world"))
+	tree.InsertString([]byte("yafim kazak tries too"))
+	tree.InsertString([]byte("Kobi has already succedd"))
 	testStrings := []string{"cgi", "xcgi", "cgi-bin"}
 	for _, testString := range testStrings {
-		word, startIndex, endIndex, isFound := tree.LookupWord([]byte(testString))
+		matches, isFound := tree.LookupString([]byte(testString))
 		if isFound {
-			fmt.Printf("Found match of substring [%v], from string [%v]; in node [%v]\n", testString[startIndex:endIndex+1], testString, word.Id)
+			for _, match := range matches {
+				fmt.Printf("Found match of substring [%v], from string [%v]; in Node [%v]\n", testString[match.StartIndex:match.EndIndex+1], testString, match.Node.Id)
+			}
 		} else {
 			fmt.Printf("Didn't find match for string - [%v]\n", testString)
 		}
 
 	}
+	vis(&tree.root)
 }
